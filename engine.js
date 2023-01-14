1ac11aa5bf621da7c46987ef6fc798dba927eb23
Arbatsky <moscowgoodhit@gmail.com>
2023-01-14 16:18:25 +0300

    Переписываю код заново. Готово добавление задачи в разметку и базу, изменение статуса в разметке и базе.

--- engine.js
+++ engine.js
@@ -1,29 +1,67 @@
-const formEl = document.querySelector('.form');
-const inputEl = document.querySelector('.input');
-const tasksListEl = document.querySelector('.tasks-list');
 
-let tasks = []; // Создаем массив с задачами
+/* Находим объекты на странице */
+const formAddTaskEl = document.querySelector('.form-add-task');
+const inputTaskNameEl = document.querySelector('#input-task-name');
+const taskListEl = document.querySelector('.task-list');
 
-formEl.addEventListener('submit', addTask); // Обработчик отправленной формы
+/* Создаем массив с задачами */
+let taskListObj = [];
 
+/* Добавление задачи */
+formAddTaskEl.addEventListener('submit', addTask); // Обработчик отправленной формы
 function addTask(event) {
     event.preventDefault(); // Сброс перезагрузки страницы при отправке формы
-    const taskText = inputEl.value; // Забираем введенный текст из инпута
-    const newTask = { // Создаем Новый таск как объект
+    const taskText = inputTaskNameEl.value; // Забираем введенный текст из инпута
+    const taskItemObj = { // Создаем Новый таск как объект
         id: Date.now(),
-        name: taskText,
+        text: taskText,
         done: false
     }
-    tasks.push(newTask) // Добавляем его в массив
-    /* Создаем и разметку и вставляем на страницу */
-    const taskHTML = `
-        <li id="${newTask.id}" class="task-item">
-            <img class="task-item__icon" src="./icons/checkbox-off.svg" width="20" height="20" alt="checkbox">
-            <p class="task-item__txt txt">${taskText}</p>
-        </li>
-    `
-    tasksListEl.insertAdjacentHTML('beforeend', taskHTML);
-    /* */
-    inputEl.value = '';
-    inputEl.focus();
+    taskListObj.push(taskItemObj); // Добавляем его в массив
+    renderTaskItemEl(taskItemObj); // Отрисовываем таск в HTML
+    inputTaskNameEl.value = ''; // Очищаем инпут
+    /*inputTaskNameEl.focus(); // Возвращаем фокус на инпут*/
 }
+
+/* Отрисовка таска */
+function renderTaskItemEl(taskItemObj) {
+    const imgState = taskItemObj.done ? './icons/checkbox-on.svg' : './icons/checkbox-off.svg';
+    const pState = taskItemObj.done ? '.txt_checked' : '';
+    const taskHTML =
+        `<li id="${taskItemObj.id}" class="task-item">
+            <img class="task-item__icon" src="${imgState}" width="20" height="20" alt="checkbox">
+            <p class="task-item__txt txt ${pState}">${taskItemObj.text}</p>
+        </li>`;
+    taskListEl.insertAdjacentHTML('beforeend', taskHTML);
+}
+
+
+/* Смена статуса задачи */
+taskListEl.onclick = () => {
+    let taskItemsEl = document.querySelectorAll('.task-item');
+    let taskItemEl = document.querySelector('.task-item');
+    for (taskItemEl of taskItemsEl) {
+        taskItemEl.onclick = function () {
+            const taskId = Number(this.id);
+            const taskItemObj = taskListObj.find(function (taskItemObj) {
+                if (taskItemObj.id === taskId) {
+                    return true
+                }
+            })
+            taskItemObj.done = !taskItemObj.done;
+            const taskItem__iconEl = this.querySelector('.task-item__icon');
+            const taskItem__txtEl = this.querySelector('.task-item__txt');
+            if (taskItemObj.done === true) {
+                taskItem__iconEl.src = "./icons/checkbox-on.svg";
+                taskItem__txtEl.classList.add('txt_checked');
+                console.log('test1');
+            } else {
+                taskItem__iconEl.src = "./icons/checkbox-off.svg";
+                taskItem__txtEl.classList.remove('txt_checked');
+                console.log('test2');
+            }
+            console.log(this.id + taskListObj);
+        }
+    }
+}
+
